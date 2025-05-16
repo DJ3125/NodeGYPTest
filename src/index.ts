ce88f8224d0fd324f5caf9e37e3625a48572731a
DJ3125 <dylanalexja@gmail.com>
2025-05-16 06:45:55 -0500

    added module aliases

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 //import {hello} from "./../build/Release/hello.node";
-const {hello} = require("./../build/Release/hello.node");
+//const {getAbsolutePath} = require("./..");
+require("module-alias/register");
+const {hello} = require("@nodeBinaries/hello.node");
 
 console.log(hello());
